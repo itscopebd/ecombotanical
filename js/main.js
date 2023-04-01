3b4639106b02dadb8c71bab285f3eafa6ec36fcd
itscopebd <itscopebd@gmail.com>
2023-04-01 12:38:06 +0600

    slider image added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+let sliderIndex=1;
+const BannerLoad = (n) => {
+    const imgContainer=document.querySelectorAll(".slider");
+   if (n> imgContainer.length-1 ) {
+    sliderIndex=1;
+   }
+for (let i = 0; i < imgContainer.length; i++) {
+    
+    imgContainer[i].style.display="none";
+    imgContainer[i].style.transition="1s";
+
+}
+imgContainer[sliderIndex-1].style.display="block"    
+
+
+}
+
+setInterval(()=>{
+    BannerLoad(sliderIndex++);
+},3000)
