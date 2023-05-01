27b05d07eddf80c061db15d52154deb0879780cc
itscopebd <itscopebd@gmail.com>
2023-05-01 17:30:39 +0600

    carousel product add and some working move right site product

--- js/main.js
+++ js/main.js
@@ -1,21 +1,47 @@
-
-let sliderIndex=1;
+// slider 
+let sliderIndex = 1;
 const BannerLoad = (n) => {
-    const imgContainer=document.querySelectorAll(".slider");
-   if (n> imgContainer.length-1 ) {
-    sliderIndex=1;
-   }
-for (let i = 0; i < imgContainer.length; i++) {
-    
-    imgContainer[i].style.display="none";
-    imgContainer[i].style.transition="1s";
+    const imgContainer = document.querySelectorAll(".slider");
+    if (n > imgContainer.length - 1) {
+        sliderIndex = 1;
+    }
+    for (let i = 0; i < imgContainer.length; i++) {
 
-}
-imgContainer[sliderIndex-1].style.display="block"    
+        imgContainer[i].style.display = "none";
+        imgContainer[i].style.transition = "1s";
+
+    }
+    imgContainer[sliderIndex - 1].style.display = "block"
 
 
 }
 BannerLoad();
-setInterval(()=>{
+setInterval(() => {
     BannerLoad(sliderIndex++);
-},5000)
+}, 5000)
+
+
+// carousel slider 
+
+// const span=document.querySelectorAll(".newArraival__heading span")
+const carousel__product = document.querySelectorAll(".carousel__product");
+const totalProduct = carousel__product.length;
+const perPageProduct = Math.ceil(totalProduct / 4);
+
+
+let l = 0;
+const perPage = 25.34;
+const maxProductWidth=perPage * perPageProduct;
+const rightMover = () => {
+    l = l + perPage;
+    for (const i of carousel__product) {
+        if (l > maxProductWidth) {
+            l=0;
+            console.log(l)
+        }
+        i.style.left = "-" + l + "%";
+    }
+    
+}
+
+
